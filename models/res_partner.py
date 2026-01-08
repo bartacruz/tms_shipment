7684dcda07440c3e398b609cd724f2468976daa1
Julio Santa Cruz <julio@digicorp.com.ar>
2026-01-08 08:02:49 -0300

    Name alias for res.partner

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,27 +1,48 @@
-from odoo import api, fields, models
+from odoo import api, fields, models,_
 
 
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
+    _rec_names_search = ['alias_name', 'complete_name', 'email', 'ref', 'vat', 'company_registry']  # TODO vat must be sanitized the same way for storing/searching
     
-    driver_ids = fields.One2many('tms.driver', 'partner_id')
-    driver_id = fields.Many2one('tms.driver', compute='_compute_driver_id', inverse='_inverse_driver_id')
-    
-    @api.depends('driver_ids')
-    def _compute_driver_id(self):
+    alias_name = fields.Char(_('Alias'))
+
+    @api.depends('alias_name')
+    @api.depends_context('show_alias')
+    def _compute_display_name(self):
+        super()._compute_display_name()
+        print("CONTEXT", self._context.get('show_alias'))
+        if not self._context.get('show_alias'):
+            return
         for record in self:
-            if len(record.driver_ids) > 0:
-                record.driver_id = record.driver_ids[0].id
-            else:
-                record.driver_id = False
+            if record.alias_name:
+                record.display_name = "["+record.alias_name+"] " + record.display_name 
     
-    def _inverse_driver_id(self):
-        for record in self:
-            if len(record.driver_ids) > 0:
-                driver = self.env['tms.driver'].browse(record.driver_ids[0])
-                driver.partner_id = False
-            record.driver_id.partner_id=record
+    # @api.model
+    # def _name_search(self, name, domain=None, operator='ilike', limit=None, order=None):
+    #     query = super()._name_search(name, domain, operator, limit, order)
+    #     if not query:
+    #         self._refresh()
+    #         return super()._name_search(name, domain, operator, limit, order)
+    #     return query
+    # driver_ids = fields.One2many('tms.driver', 'partner_id')
+    # driver_id = fields.Many2one('tms.driver', compute='_compute_driver_id', inverse='_inverse_driver_id')
+    
+    # @api.depends('driver_ids')
+    # def _compute_driver_id(self):
+    #     for record in self:
+    #         if len(record.driver_ids) > 0:
+    #             record.driver_id = record.driver_ids[0].id
+    #         else:
+    #             record.driver_id = False
+    
+    # def _inverse_driver_id(self):
+    #     for record in self:
+    #         if len(record.driver_ids) > 0:
+    #             driver = self.env['tms.driver'].browse(record.driver_ids[0])
+    #             driver.partner_id = False
+    #         record.driver_id.partner_id=record
                                                         
 
     def toggle_location(self):
