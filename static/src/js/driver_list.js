9e7ab67c74bac85dba1c1099376020e0414f877a
Julio Santa Cruz <bartacruz@gmail.com>
2026-02-11 12:06:14 -0300

    Drivers list show drivers not in base as muted

--- static/src/js/driver_list.js
+++ static/src/js/driver_list.js
@@ -17,13 +17,14 @@ export class DriverList extends Component {
 
     setup() {
         this.orm = useService("orm");
-
+        this.action = useService("action");
         this.partners = useState({ data: [] });
         this.pager = useState({ offset: 0, limit: 50 });
         this.keepLast = new KeepLast();
         this.state = useState({
             searchString: "",
             displayActiveDrivers: false,
+            lastSearch: "",
         })
         this.busService = this.env.services.bus_service;
         this.busService.addChannel("drivers");
@@ -34,9 +35,7 @@ export class DriverList extends Component {
         this.busService.start();
 
         onWillStart(async () => {
-            const { length, records } = await this.loadDrivers();
-            this.partners.data = records;
-            this.pager.total = length;
+            await this.updateDrivers();
         })
         this.onDrag = function(ev){
             console.debug("onDrag",this,ev);
@@ -54,21 +53,46 @@ export class DriverList extends Component {
         // });
         console.debug("env:",this.env.services);
     }
-
+    async selectDriver(ev) {
+        const driver_id = $(ev.srcElement).closest('td').dataset.driverId;
+        console.debug("selectDriver",driver_id,this,ev);
+        
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            name: 'Chofer',
+            target: 'current',
+            res_id: driver_id,
+            res_model: 'tms.driver',
+            views: [[false, 'form']],
+        });
+    }
     get displayedPartners() {
-        return this.filterDrivers(this.state.searchString);
+        // return this.filterDrivers(this.state.searchString);
+        console.debug("getdisplayedPartners",this.state.searchString,this.state.lastSearch);
+        if (this.state.searchString != this.state.lastSearch) {
+            this.updateDrivers();
+        }
+        return this.partners.data;
     }
 
-    async onChangeActiveDrivers(ev) {
-        this.state.displayActiveDrivers = ev.target.checked;
-        this.partners.data = await this.keepLast.add(this.loadDrivers());
-        this.pager.offset = 0;
-        const { length, records } = await this.keepLast.add(this.loadDrivers());
+    async updateDrivers() {
+        const { length, records } = await this.loadDrivers();
         this.partners.data = records;
         this.pager.total = length;
     }
 
+    async onChangeActiveDrivers(ev) {
+        this.state.displayActiveDrivers = ev.target.checked;
+        this.updateDrivers();
+        // this.partners.data = await this.keepLast.add(this.loadDrivers());
+        // this.pager.offset = 0;
+        // const { length, records } = await this.keepLast.add(this.loadDrivers());
+        // this.partners.data = records;
+        // this.pager.total = length;
+    }
+
     filterDrivers(name) {
+        console.debug("filterDrivers",name);
         if (name) {
             return fuzzyLookup(name, this.partners.data, (partner) => partner.display_name);
         } else {
@@ -77,12 +101,18 @@ export class DriverList extends Component {
     }
 
     loadDrivers() {
-        console.debug("loading drivers");
         const { limit, offset } = this.pager;
-        const domain = this.state.displayActiveDrivers ? [["stage_id", "=", 5]] : [];
+        const domain = this.state.displayActiveDrivers ? [["stage_id", "in", [5] ]] : [];
+        if (this.state.searchString.length > 2) {
+            domain.push(['name','ilike',this.state.searchString]);
+        }
+        this.state.lastSearch = this.state.searchString;
+        console.debug("loading drivers", domain);
+        
         return this.orm.webSearchRead("tms.driver", domain, {
             specification: {
                 "display_name": {},
+                "stage_id": { fields: { name: {} } },
             },
             limit,
             offset,
@@ -94,6 +124,6 @@ export class DriverList extends Component {
         Object.assign(this.pager, newState);
         const { records } = await this.loadDrivers();
         this.partners.data = records;
-        this.filterDrivers(this.filterName);
+        // this.filterDrivers(this.filterName);
     }
 }
