539d8c8d46452c914f45ffa93e21c350ee3d3426
Julio Santa Cruz <bartacruz@gmail.com>
2026-02-26 22:49:56 -0300

    Fix logging

--- models/mail_message.py
+++ models/mail_message.py
@@ -11,15 +11,16 @@ class MailMessage(models.Model):
     tms_order_id = fields.Many2one('tms.order',_('Related TMS Order'))
     
     def write(self, vals):
-        super().write(vals)
+        ret = super().write(vals)
         if 'gateway_message_id' in vals and self.body.find("Button:"):
-            _logger.warning('WA write: %s detecte un boton',self)
+            _logger.warning('WA write: %s detecte un boton %s || %s',self,vals['gateway_message_id'],self.gateway_message_id)
             template_message = self.gateway_message_id
-            _logger.warning('WA write: el mensaje es',template_message)
+            _logger.warning('WA write: el mensaje es %s',template_message)
             tms_order = template_message.tms_order_id
-            _logger.warning('WA write: la orden es',tms_order)
+            _logger.warning('WA write: la orden es %s',tms_order)
             if self.body.find("Confirmar"):
                 tms_order.stage_id = self.env.ref("tms.tms_stage_order_confirmed")
+        return ret
             
             
     
